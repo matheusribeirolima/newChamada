6cd767a4e8163bcc07841cf539f5422c164457cd
Matheus Ribeiro <p000matheus.lima@prservicos.com.br>
2018-12-14 16:27:40 -0200

    project added

---
+++ app/src/main/java/com/example/matheus/chamada/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.matheus.chamada
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
