fe764092186d804c6b2cbbf72c1f56fda517d0d4
Matheus Ribeiro <p000matheus.lima@prservicos.com.br>
2018-12-15 13:26:39 -0200

    project dependencies updated

--- app/src/main/java/com/example/matheus/chamada/MainActivity.kt
+++ app/src/main/java/com/example/matheus/chamada/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.matheus.chamada
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
